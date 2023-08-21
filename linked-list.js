1bb986a701b2240430cfe57eb65267e70d5ae90d
Filomn Abrha <filmonabrha2020@gmail.com>
2023-08-21 16:07:01 +0300

    Create an append method to append a node to the end of the list

--- linked-list.js
+++ linked-list.js
@@ -6,10 +6,24 @@ const Node = (value = null, next = null) => {
 const LinkedLists = () => {
     let head = null; //{value: 1, next: {value:2, next: {value:3, next: null}}}
     let count = 0;
-
+    // appends a new value to the end of the link
+    const append = (newItem, headNode = null) => {
+        if (headNode === null) {
+            if (head === null) {
+                return (head = Node(newItem));
+            } else {
+                return append(newItem, head);
+            }
+        }
+        headNode.next = Node(newItem);
+    };
     const gethead = () => head;
 
-    return { gethead };
+    return { append, gethead };
 };
 
 const list = LinkedLists();
+list.append(1);
+list.append(2);
+list.append(3);
+console.log(list.gethead());
