cf3d6138f4295320bb71575b515ec0561331ee9a
Filomn Abrha <filmonabrha2020@gmail.com>
2023-08-21 16:14:03 +0300

    Create an prepend  method to append a node to the start of the list

--- linked-list.js
+++ linked-list.js
@@ -6,24 +6,43 @@ const Node = (value = null, next = null) => {
 const LinkedLists = () => {
     let head = null; //{value: 1, next: {value:2, next: {value:3, next: null}}}
     let count = 0;
-    // appends a new value to the end of the link
+
+    //  adds a new node containing newItem to the end of the list
     const append = (newItem, headNode = null) => {
         if (headNode === null) {
             if (head === null) {
+                count++;
                 return (head = Node(newItem));
             } else {
                 return append(newItem, head);
             }
         }
+        count++;
         headNode.next = Node(newItem);
     };
+    //adds a new node containing newItem to the start of the list
+    const prepend = (newItem, headNode = null) => {
+        if (headNode === null) {
+            if (head === null) {
+                count++;
+                return (head = Node(newItem));
+            } else {
+                return prepend(newItem, head);
+            }
+        }
+        count++;
+        head = Node(newItem, head);
+    };
     const gethead = () => head;
+    const getSize = () => count;
 
-    return { append, gethead };
+    return { append, prepend, gethead, getSize };
 };
 
 const list = LinkedLists();
 list.append(1);
 list.append(2);
 list.append(3);
+list.prepend(0);
 console.log(list.gethead());
+console.log(list.getSize());
